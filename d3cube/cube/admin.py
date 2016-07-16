49952119c4cada8c92ecb52818b2ed3a65839226
James Fourman <zerocooldoh@gmail.com>
2016-07-16 17:15:58 -0400

    More commits

--- d3cube/cube/admin.py
+++ d3cube/cube/admin.py
@@ -3,3 +3,5 @@ from . import models
 
 # Register your models here.
 admin.site.register(models.Cube)
+admin.site.register(models.CubeType)
+admin.site.register(models.CharacterType)
--- d3cube/cube/views.py
+++ d3cube/cube/views.py
@@ -4,7 +4,22 @@ from . import models, serializers
 
 # Create your views here.
 def default(request):
-    return render(request, 'cube/default.html', {'items': models.Cube.objects.all()})
+    ct_filter = request.GET.get('filter', None)
+    cube_type = request.GET.get('ct', None)
+    in_cube = request.GET.get('in_cube', None)
+
+    queryset = models.Cube.objects.all()
+    if ct_filter:
+        queryset = queryset.filter(character_type__short_name=ct_filter)
+
+    if cube_type:
+        queryset = queryset.filter(cube_slot__name=cube_type)
+
+    if in_cube:
+        print(in_cube)
+        queryset = queryset.filter(in_cube=in_cube)
+
+    return render(request, 'cube/default.html', {'items': queryset})
 
 
 class Update(generics.UpdateAPIView):
