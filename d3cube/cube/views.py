648e4c07f299865f51cb0b131ad091ee1725e3c1
James Fourman <zerocooldoh@gmail.com>
2016-07-16 22:02:42 -0400

    Added ability to sort

--- d3cube/cube/views.py
+++ d3cube/cube/views.py
@@ -4,16 +4,18 @@ from . import models, serializers
 
 # Create your views here.
 def default(request):
-    ct_filter = request.GET.get('filter', None)
-    cube_type = request.GET.get('ct', None)
-    in_cube = request.GET.get('in_cube', None)
+    character_type = request.GET.get('characterType', None)
+    cube_type = request.GET.get('cubeType', None)
+    in_cube = request.GET.get('showCubedItems', None)
 
     queryset = models.Cube.objects.all()
-    if ct_filter:
-        queryset = queryset.filter(character_type__short_name=ct_filter)
+    if character_type:
+        character_type = int(character_type)
+        queryset = queryset.filter(character_type__id=character_type)
 
     if cube_type:
-        queryset = queryset.filter(cube_slot__name=cube_type)
+        cube_type = int(cube_type)
+        queryset = queryset.filter(cube_slot__id=cube_type)
 
     if in_cube:
         if in_cube == "True":
@@ -21,10 +23,18 @@ def default(request):
         else:
             in_cube = False
 
-        queryset = queryset.filter(in_cube=in_cube)       
+        queryset = queryset.filter(in_cube=in_cube)
 
+    context = {
+        'items': queryset,
+        'character_type': character_type,
+        'cube_type': cube_type,
+        'in_cube': in_cube,
+        'character_type_list': models.CharacterType.objects.all().order_by('name'),
+        'cube_type_list': models.CubeType.objects.all().order_by('name'),
+    }
 
-    return render(request, 'cube/default.html', {'items': queryset})
+    return render(request, 'cube/default.html', context)
 
 
 class Update(generics.UpdateAPIView):
